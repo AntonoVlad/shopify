79e05697ed95b790e65835fe3bc2d6a184ad0d7a
AntonoVlad <65464656+AntonoVlad@users.noreply.github.com>
2020-06-24 16:54:23 +0300

    finish slider js

---
+++ main.js
@@ -0,0 +1,23 @@
+let images = document.querySelectorAll(".reviews-img-wrapper img")
+let imgIndex = 0
+let rightArrow = document.querySelector(".reviews-slider-right")
+rightArrow.onclick = function () {
+    if (imgIndex < images.length - 1) imgIndex = imgIndex + 1
+    toggleImages()
+}
+let leftArrow = document.querySelector(".reviews-slider-left")
+leftArrow.onclick = function () {
+    if (imgIndex > 0)imgIndex = imgIndex - 1
+    toggleImages()
+}
+
+function toggleImages() {
+    for (let i = 0; i < images.length; i++) {
+        let img = images[i]
+        if (i === imgIndex) {
+            img.classList.remove("hidden")
+        } else {
+            img.classList.add("hidden")
+        }
+    }
+}
